started 3, set up plots for 3. fixed some units issues with 1

diskModel.py
@@ -11,28 +11,28 @@ Rsun = 6.9599e10 #cm
 def calcF(T,Rs,Dp):
     def Fnu(nu):
         Inu = ((2.*h*nu**3)/(c**2))*(1./(np.exp((h*nu)/(k*T))-1.))
-        Ar = (4.*np.pi*Rs**2)/(4.*np.pi*Dp**2)
         Omg = np.pi*(Rs/Dp)**2
-        return Inu*Omg*Ar/Jy
+        return Inu*Omg/Jy
     return Fnu
 
 def Pin(Fnu,rg):
     if rg == 0.1:
         Qdat=ascii.read("suvSil_21",header_start=2436,data_start=2437,data_end=2677)
-
     elif rg == 1.:
         Qdat=ascii.read("suvSil_21",header_start=3651,data_start=3656,data_end=3892)
-
     elif rg == 10.:
         Qdat=ascii.read("suvSil_21",header_start=4866,data_start=4867,data_end=5107)
-
     elif rg == 1000.:
         Qdat=ascii.read("suvSil_21",header_start=4866,data_start=4867,data_end=5107)
         Qdat['Q_abs']=1.
-
     else:
         raise Error("Grain radius must be 0.1, 1, 10, or 1000 um")
 
     Pin = (np.pi*(rg*1e-4)**2)*np.trapz(Qdat['Q_abs']*Fnu(c/Qdat['w(micron)'])*Jy,x=c/Qdat['w(micron)'])
 
     return Pin
+
+def Teq(Pin,rg):
+    #Pin = Pout = (4.*np.pi*rg**2)*Q*sigma*T**4
+    return 0.
+

plots.py
@@ -25,14 +25,44 @@ nu=c/l
 plt.plot(np.log10(nu),np.log10(Fnu130AU(nu)),label="130 AU")
 plt.legend(loc=2)
 plt.savefig("FomFnu.pdf")
+plt.clf()
 
 ######## 2 ########
-print "Pin for 0.1 um grain at 10 AU: ", dM.Pin(Fnu10AU,0.1)
-print "Pin for 1 um grain at 10 AU: ", dM.Pin(Fnu10AU,1.)
-print "Pin for 10 um grain at 10 AU: ", dM.Pin(Fnu10AU,10.)
-print "Pin for 1 mm grain at 10 AU: ", dM.Pin(Fnu10AU,1000.)
+rg = np.array([0.1,1.,10.,1000.])
+Pin10AU = np.array([dM.Pin(Fnu10AU,i) for i in rg])
+Pin130AU = np.array([dM.Pin(Fnu130AU,i) for i in rg])
 
-print "Pin for 0.1 um grain at 130 AU: ", dM.Pin(Fnu130AU,0.1)
-print "Pin for 1 um grain at 130 AU: ", dM.Pin(Fnu130AU,1.)
-print "Pin for 10 um grain at 130 AU: ", dM.Pin(Fnu130AU,10.)
-print "Pin for 1 mm grain at 130 AU: ", dM.Pin(Fnu130AU,1000.)
+print "Pin for 0.1 um grain at 10 AU: ", Pin10AU[0]
+print "Pin for 1 um grain at 10 AU: ", Pin10AU[1]
+print "Pin for 10 um grain at 10 AU: ", Pin10AU[2]
+print "Pin for 1 mm grain at 10 AU: ", Pin10AU[3]
+
+print "Pin for 0.1 um grain at 130 AU: ", Pin130AU[0]
+print "Pin for 1 um grain at 130 AU: ", Pin130AU[1]
+print "Pin for 10 um grain at 130 AU: ", Pin130AU[2]
+print "Pin for 1 mm grain at 130 AU: ", Pin130AU[3]
+
+####### 3 ########
+Teq10AU = np.array([dM.Teq(Pin10AU[i],rg[i]) for i in range(4)])
+Teq130AU = np.array([dM.Teq(Pin130AU[i],rg[i]) for i in range(4)])
+
+Fnu10AUp1 = 
+Fnu10AU1 = 
+Fnu10AU10 = 
+Fnu10AU1mm = 
+
+plt.plot(np.log10(nu),np.log10(Fnu10AUp1(nu)),label=r"$a=0.1\mu\mathrm{m}")
+plt.plot(np.log10(nu),np.log10(Fnu10AU1(nu)),label=r"$a=1\mu\mathrm{m}")
+plt.plot(np.log10(nu),np.log10(Fnu10AU10(nu)),label=r"$a=10\mu\mathrm{m}")
+plt.plot(np.log10(nu),np.log10(Fnu10AU1mm(nu)),label=r"$a=1\mathrm{mm}")
+
+
+Fnu130AUp1 = 
+Fnu130AU1 = 
+Fnu130AU10 = 
+Fnu130AU1mm = 
+
+plt.plot(np.log10(nu),np.log10(Fnu130AUp1(nu)),label=r"$a=0.1\mu\mathrm{m}")
+plt.plot(np.log10(nu),np.log10(Fnu130AU1(nu)),label=r"$a=1\mu\mathrm{m}")
+plt.plot(np.log10(nu),np.log10(Fnu130AU10(nu)),label=r"$a=10\mu\mathrm{m}")
+plt.plot(np.log10(nu),np.log10(Fnu130AU1mm(nu)),label=r"$a=1\mathrm{mm}")
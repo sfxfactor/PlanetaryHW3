computed Teq. fixed some units things in previous parts

diskModel.py
@@ -1,5 +1,6 @@
 import numpy as np
 from astropy.io import ascii
+import scipy.optimize as opt
 
 h = 6.626e-27 #erg s
 c = 2.99792458e10 #cm s^-1
@@ -8,11 +9,14 @@ Jy = 1e-23 # erg s^-1 cm^-2 Hz^-1
 AU = 1.496e13 #cm
 Rsun = 6.9599e10 #cm
 
+def Bnu(nu,T):
+    return ((2.*h*nu**3)/(c**2))*(1./(np.exp((h*nu)/(k*T))-1))
+
+
 def calcF(T,Rs,Dp):
     def Fnu(nu):
-        Inu = ((2.*h*nu**3)/(c**2))*(1./(np.exp((h*nu)/(k*T))-1.))
         Omg = np.pi*(Rs/Dp)**2
-        return Inu*Omg/Jy
+        return Bnu(nu,T)*Omg/Jy
     return Fnu
 
 def Pin(Fnu,rg):
@@ -28,11 +32,28 @@ def Pin(Fnu,rg):
     else:
         raise Error("Grain radius must be 0.1, 1, 10, or 1000 um")
 
-    Pin = (np.pi*(rg*1e-4)**2)*np.trapz(Qdat['Q_abs']*Fnu(c/Qdat['w(micron)'])*Jy,x=c/Qdat['w(micron)'])
+    Pin = (np.pi*(rg*1e-4)**2)*np.trapz(Qdat['Q_abs']*Fnu(c/(Qdat['w(micron)']*1e-6))*Jy,x=c/(Qdat['w(micron)']*1e-6))
 
     return Pin
 
 def Teq(Pin,rg):
     #Pin = Pout = (4.*np.pi*rg**2)*Q*sigma*T**4
-    return 0.
+    if rg == 0.1:
+        Qdat=ascii.read("suvSil_21",header_start=2436,data_start=2437,data_end=2677)
+    elif rg == 1.:
+        Qdat=ascii.read("suvSil_21",header_start=3651,data_start=3656,data_end=3892)
+    elif rg == 10.:
+        Qdat=ascii.read("suvSil_21",header_start=4866,data_start=4867,data_end=5107)
+    elif rg == 1000.:
+        Qdat=ascii.read("suvSil_21",header_start=4866,data_start=4867,data_end=5107)
+        Qdat['Q_abs']=1.
+    else:
+        raise Error("Grain radius must be 0.1, 1, 10, or 1000 um")
+
+    def f(T):
+        return (4.*np.pi*rg**2)*np.trapz(Qdat['Q_abs']*Bnu(c/(Qdat['w(micron)']*1e-6),T)*np.pi,x=c/(Qdat['w(micron)']*1e-6))-Pin
+
+    Teq = opt.root(f,300.).x[0]
+    
+    return Teq
 

plots.py
@@ -44,25 +44,27 @@ print "Pin for 1 mm grain at 130 AU: ", Pin130AU[3]
 
 ####### 3 ########
 Teq10AU = np.array([dM.Teq(Pin10AU[i],rg[i]) for i in range(4)])
+print Teq10AU
 Teq130AU = np.array([dM.Teq(Pin130AU[i],rg[i]) for i in range(4)])
+print Teq130AU
 
-Fnu10AUp1 = 
-Fnu10AU1 = 
-Fnu10AU10 = 
-Fnu10AU1mm = 
+#Fnu10AUp1 = 
+#Fnu10AU1 = 
+#Fnu10AU10 = 
+#Fnu10AU1mm = 
 
-plt.plot(np.log10(nu),np.log10(Fnu10AUp1(nu)),label=r"$a=0.1\mu\mathrm{m}")
-plt.plot(np.log10(nu),np.log10(Fnu10AU1(nu)),label=r"$a=1\mu\mathrm{m}")
-plt.plot(np.log10(nu),np.log10(Fnu10AU10(nu)),label=r"$a=10\mu\mathrm{m}")
-plt.plot(np.log10(nu),np.log10(Fnu10AU1mm(nu)),label=r"$a=1\mathrm{mm}")
+#plt.plot(np.log10(nu),np.log10(Fnu10AUp1(nu)),label=r"$a=0.1\mu\mathrm{m}")
+#plt.plot(np.log10(nu),np.log10(Fnu10AU1(nu)),label=r"$a=1\mu\mathrm{m}")
+#plt.plot(np.log10(nu),np.log10(Fnu10AU10(nu)),label=r"$a=10\mu\mathrm{m}")
+#plt.plot(np.log10(nu),np.log10(Fnu10AU1mm(nu)),label=r"$a=1\mathrm{mm}")
 
 
-Fnu130AUp1 = 
-Fnu130AU1 = 
-Fnu130AU10 = 
-Fnu130AU1mm = 
+#Fnu130AUp1 = 
+#Fnu130AU1 = 
+#Fnu130AU10 = 
+#Fnu130AU1mm = 
 
-plt.plot(np.log10(nu),np.log10(Fnu130AUp1(nu)),label=r"$a=0.1\mu\mathrm{m}")
-plt.plot(np.log10(nu),np.log10(Fnu130AU1(nu)),label=r"$a=1\mu\mathrm{m}")
-plt.plot(np.log10(nu),np.log10(Fnu130AU10(nu)),label=r"$a=10\mu\mathrm{m}")
-plt.plot(np.log10(nu),np.log10(Fnu130AU1mm(nu)),label=r"$a=1\mathrm{mm}")
+#plt.plot(np.log10(nu),np.log10(Fnu130AUp1(nu)),label=r"$a=0.1\mu\mathrm{m}")
+#plt.plot(np.log10(nu),np.log10(Fnu130AU1(nu)),label=r"$a=1\mu\mathrm{m}")
+#plt.plot(np.log10(nu),np.log10(Fnu130AU10(nu)),label=r"$a=10\mu\mathrm{m}")
+#plt.plot(np.log10(nu),np.log10(Fnu130AU1mm(nu)),label=r"$a=1\mathrm{mm}")